5027f8a39e04fab34ee5c7ca7b6e13a0d1bdaf07
Nashke <nahashon.kamau@student.moringaschool.com>
2022-01-12 22:31:58 +0300

    change variables

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function myButton(event) {
     event.preventDefault()
-bday = window.prompt("Please enter your birthdate (yyyy-mm-dd)?","");
 
 var day = bdayArray[2];
 var month = bdayArray [1];
@@ -20,5 +19,24 @@ console.log(century);
 
 var bornyear = year.slice (2,5);
 console.log(bornyear);
+
+var century = document.getElementById('century').value
+console.log(century)
+var year = document.getElementById('year').value
+console.log(year)
+var month = parseInt(document.getElementById('month').value)
+console.log(month)
+var date = parseInt(document.getElementById('date').value)
+console.log(date)
+var gender = document.querySelector('input[name = gender]:checked').value
+console.log(gender)
+
+var CC = parseInt(century);
+var YY = parseInt(bornyear);
+var MM = parseInt( month);
+var DD = parseInt( day);
+
+var dayOfWeek =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+
 
 
