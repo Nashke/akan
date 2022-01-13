7140a831f4d99504a84b8f5a78c79d0768990d20
Nashke <nahashon.kamau@student.moringaschool.com>
2022-01-13 06:40:33 +0300

    html layout change

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-function myButton(event) {
-    event.preventDefault()
-
+bday= window.prompt("when is your birthday (yyyy-mm-dd)?", "")
 var day = bdayArray[2];
 var month = bdayArray [1];
 var year = bdayArray [0];
@@ -20,23 +18,25 @@ console.log(century);
 var bornyear = year.slice (2,5);
 console.log(bornyear);
 
-var century = document.getElementById('century').value
-console.log(century)
-var year = document.getElementById('year').value
-console.log(year)
-var month = parseInt(document.getElementById('month').value)
-console.log(month)
-var date = parseInt(document.getElementById('date').value)
-console.log(date)
-var gender = document.querySelector('input[name = gender]:checked').value
-console.log(gender)
-
 var CC = parseInt(century);
 var YY = parseInt(bornyear);
 var MM = parseInt( month);
 var DD = parseInt( day);
 
-var dayOfWeek =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+var dayOfWeek   =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+
+function class date(dw){
+    switch (dw) {
+        case 1:
+            alert("birthday is on Sunday ,akan name is Kwasi")
+            break;
+        case 2:
+            alert("birthday is on Monday,Akan name is Kwado")
+    
+        default:
+            break;
+    }
+}
 
 
 
